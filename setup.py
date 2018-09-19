b75207d045d5aaf2184bbeddaca56e22b5027599
AlliedToasters <michael.r.klear@gmail.com>
2018-09-19 20:43:14 +0200

    fix dependencies

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='space_bandits',
-    version='0.0.1',
+    version='0.0.2',
     description='Contextual Bandits Library',
     author='Michael Klear',
     author_email='michael@launchpad.ai',
-    url='https://github.com/AlliedToasters/space_bandits/archive/v0.0.1.tar.gz',
+    url='https://github.com/AlliedToasters/space_bandits/archive/v0.0.2.tar.gz',
     install_requires=[
-      'tensorflow>=1.5.0'
+      'tensorflow>=1.5.0',
       'numpy>=1.14.3',
       'scipy>=0.19.1'
     ],
