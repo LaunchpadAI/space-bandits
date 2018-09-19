66503505426764ccbaf9819a50e69d288e927f33
AlliedToasters <michael.r.klear@gmail.com>
2018-09-19 20:37:52 +0200

    update setup.py for first release

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description='Contextual Bandits Library',
     author='Michael Klear',
     author_email='michael@launchpad.ai',
+    url='https://github.com/AlliedToasters/space_bandits/archive/v0.0.1.tar.gz',
     install_requires=[
       'tensorflow>=1.5.0'
       'numpy>=1.14.3',
