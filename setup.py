f8f78e77c5a7c9f485770dfda4e2ddc7664c3031
AlliedToasters <michael.r.klear@gmail.com>
2019-05-19 17:16:37 -0600

    update setup.py for v0.0.95

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ Supports both online learning and offline training of models as well as novel me
 
 setup(
     name='space-bandits',
-    version='0.0.94',
+    version='0.0.95',
     description='Deep Bayesian Contextual Bandits Library',
     long_description=long_desc,
     author='Michael Klear',
     author_email='michael@fellowship.ai',
     url='https://github.com/fellowship/space-bandits',
-    download_url='https://github.com/fellowship/space-bandits/archive/v0.0.94.tar.gz',
+    download_url='https://github.com/fellowship/space-bandits/archive/v0.0.95.tar.gz',
     install_requires=[
       'numpy>=1.14.5',
       'scipy>=0.19.1',
