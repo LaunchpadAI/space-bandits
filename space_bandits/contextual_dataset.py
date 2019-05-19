fix issues with scaling inputs to neural network

space_bandits/contextual_dataset.py
@@ -194,7 +194,7 @@ class ContextualDataset(object):
         means = self.contexts.mean(dim=0)
         stds = self.contexts.std(dim=0)
         stds[stds==0] = 1
-        self.scaled_contexts = self.contexts
+        self.scaled_contexts = self.contexts.clone()
         for col in range(self._context_dim):
             self.scaled_contexts[:, col] -= means[col]
             self.scaled_contexts[:, col] /= stds[col]

space_bandits/neural_bandit_model.py
@@ -56,6 +56,7 @@ class NeuralBanditModel(nn.Module):
         self.layers = []
         self.build_model()
         self.optim = self.select_optimizer()
+        self.loss = nn.modules.loss.MSELoss()
 
     def build_layer(self, inp_dim, out_dim):
         """Builds a layer with input x; dropout and layer norm if specified."""
@@ -116,6 +117,24 @@ class NeuralBanditModel(nn.Module):
         for layer in self.layers:
             nn.init.uniform_(layer.weight, a=-init_s, b=init_s)
 
+    def do_step(self, x, y, w, step):
+
+        decay_rate = self.hparams.get('lr_decay_rate', 0.5)
+        base_lr = self.hparams.get('initial_lr', 0.1)
+
+        lr = base_lr * (1 / (1 + (decay_rate * step)))
+        self.assign_lr(lr)
+
+        y_hat = self.forward(x.float())
+        y_hat *= w
+        ls = self.loss(y_hat, y.float())
+        ls.backward()
+        clip = self.hparams['max_grad_norm']
+        torch.nn.utils.clip_grad_norm_(self.parameters(), clip)
+
+        self.optim.step()
+        self.optim.zero_grad()
+
     def train(self, data, num_steps):
         """Trains the network for num_steps, using the provided data.
         Args:
@@ -126,28 +145,12 @@ class NeuralBanditModel(nn.Module):
         if self.verbose:
             print("Training {} for {} steps...".format(self.name, num_steps))
 
-        decay_rate = self.hparams.get('lr_decay_rate', 0.5)
-        base_lr = self.hparams.get('initial_lr', 0.1)
         batch_size = self.hparams.get('batch_size', 512)
 
-        loss = nn.modules.loss.MSELoss()
         data.scale_contexts()
         for step in range(num_steps):
-            lr = base_lr * (1 / (1 + (decay_rate * step)))
-            self.assign_lr(lr)
-
-            ctx, r, act = data.get_batch_with_weights(batch_size, scaled=True)
-
-            r_hat = self.forward(ctx.float())
-            r_hat *= act
-            ls = loss(r_hat, r.float())
-            print(ls.item())
-            ls.backward()
-            clip = self.hparams['max_grad_norm']
-            torch.nn.utils.clip_grad_norm_(self.parameters(), clip)
-
-            self.optim.step()
-            self.optim.zero_grad()
+            x, y, w = data.get_batch_with_weights(batch_size, scaled=True)
+            self.do_step(x, y, w, step)
 
     def get_representation(self, contexts):
         """

space_bandits/neural_linear.py
@@ -235,7 +235,7 @@ class NeuralBandits(BanditAlgorithm):
         z = self.bnn.get_representation(c)
         return z
 
-    def expected_values(self, context):
+    def expected_values(self, context, scale=False):
         """
         Computes expected values from context. Does not consider uncertainty.
         Args:
@@ -243,6 +243,9 @@ class NeuralBandits(BanditAlgorithm):
         Returns:
           expected reward vector.
         """
+        if scale:
+            context = self.data_h.scale_contexts(contexts=context)
+
         # Compute last-layer representation for the current context
         z_context = self.get_representation(context).numpy()
 
@@ -255,9 +258,6 @@ class NeuralBandits(BanditAlgorithm):
 
     def _sample(self, context, parallelize=False, n_threads=-1):
         # Sample sigma2, and beta conditional on sigma2
-        context = context.reshape(-1, self.hparams['context_dim'])
-        if self.do_scaling:
-            context = self.data_h.scale_contexts(contexts=context)
         n_rows = len(context)
         d = self.mu[0].shape[0]
         a_projected = np.repeat(np.array(self.a)[np.newaxis, :], n_rows, axis=0)
@@ -302,11 +302,13 @@ class NeuralBandits(BanditAlgorithm):
 
     def action(self, context):
         """Samples beta's from posterior, and chooses best action accordingly."""
-
         # Round robin until each action has been selected "initial_pulls" times
         if self.t < self.num_actions * self.initial_pulls:
             return self.t % self.num_actions
         else:
+            context = context.reshape(-1, self.hparams['context_dim'])
+            if self.do_scaling:
+                context = self.data_h.scale_contexts(contexts=context)
             vals = self._sample(context)
         return np.argmax(vals)
 
@@ -403,6 +405,10 @@ class NeuralBandits(BanditAlgorithm):
 
     def predict(self, contexts, thompson=True, parallelize=True, n_threads=-1):
         """Takes a list or array-like of contexts and batch predicts on them"""
+        contexts = contexts.reshape(-1, self.hparams['context_dim'])
+        if self.do_scaling:
+            contexts = self.data_h.scale_contexts(contexts=contexts)
+
         if thompson:
             reward_matrix = self._sample(contexts, parallelize=parallelize, n_threads=n_threads)
         else: